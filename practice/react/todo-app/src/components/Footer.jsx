e2355a6e80f0dd07d4f3c14364abc926e2c12a5a
Aashir Razzaq <aashir.razzaq10@gmail.com>
2024-02-25 23:22:51 +0500

    add footer buttons

--- practice/react/todo-app/src/components/Footer.jsx
+++ practice/react/todo-app/src/components/Footer.jsx
@@ -1,6 +1,33 @@
+import { useRef } from "react";
+
 export default function Footer(){
+    const buttonRef = useRef(null); 
+
+    function handleClick(event){
+        buttonRef.current?.classList.remove('active');
+        buttonRef.current = event.target;
+        buttonRef.current.classList.add('active'); 
+    }
+
     return (
         <>
+            <footer className="wrapper flex justify-center">
+                <button 
+                    onClick={handleClick}
+                    className="btn-color">
+                    All
+                </button>
+                <button
+                    onClick={handleClick}
+                    className="btn-color">
+                        Active
+                </button>
+                <button
+                    onClick={handleClick}
+                    className="btn-color">
+                        Completed
+                </button>
+            </footer>
         </>
     )
 }
