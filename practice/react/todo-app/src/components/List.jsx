8ab50256b294b13d7edf2b60afe8a73544027f02
Aashir Razzaq <aashir.razzaq10@gmail.com>
2024-02-26 22:04:57 +0500

    added list placeholders and custom scrollbar for the list

--- practice/react/todo-app/src/components/List.jsx
+++ practice/react/todo-app/src/components/List.jsx
@@ -1,23 +1,36 @@
 import ListItem from "./ListItem";
+import './List.css';
 
 export default function List({todos, filter}){
-    return (
-            <ul className="wrapper">
+    const filteredTodos = 
+    todos.filter((todo => {
+        if(filter == 'all')
+            return todo;
+        else if(filter == 'completed'){
+            return todo.completed;
+        }
+        else if(filter == 'active')
+            return !todo.completed;
+    }));
+
+    if(todos.length == 0)
+        return (
+            <div className="wrapper pad-m text-center">
+                <p className="color-gray">No work to do!</p>
+            </div>
+        )
+    else
+        return (
+            <ul className="wrapper list">
                 { 
-                    todos
-                    .filter((todo => {
-                        if(filter == 'all')
-                            return todo;
-                        else if(filter == 'completed'){
-                            return todo.completed;
-                        }
-                        else if(filter == 'active')
-                            return !todo.completed;
-                    }))
-                    .map(todo => 
+                    filteredTodos.length > 0 ? 
+                    filteredTodos.map(todo => 
                         <ListItem key={todo.id} todo={todo} />
-                    )
+                    ) : 
+                    <div className="wrapper pad-m text-center">
+                        <p className="color-gray">List is empty!</p>
+                    </div>
                 }
             </ul>
-    )
+        )
 }
